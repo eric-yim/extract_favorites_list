9cc0de6fb9e663e15e47a3b4c9a3b95ba9be3680
Eric Yim <erichyim@gmail.com>
2023-10-31 20:35:08 -0700

    up

---
+++ README.md
@@ -0,0 +1,19 @@
+## Download Videos
+
+This repo is code for the following video:
+
+- TODO
+
+This is only a demo. This package will not have support.
+## Installation
+
+```
+pip install bs4
+```
+
+## Running
+Go to favorites. Download the html code for your favorites page. Copy it to a txt file, for example ```MY_FILE.html```
+
+```
+python3 get_download_list.py --html_txt MY_FILE.html
+```
---
+++ get_download_list.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import argparse
+import re
+# COMMENT_CONTAINER = ['div',re.compile('user-post-item')]
+COMMENT_CONTAINER = ['div',{'data-e2e':'favorites-item'}]
+def ap():
+    parser = argparse.ArgumentParser(description='A simple CLI tool.')
+
+    # Add an argument
+    parser.add_argument('--html_txt', type=str, help='local path to downloaded html code' ,required=True)
+    parser.add_argument('--output_txt', type=str, help='save list to txt' ,default='download_list.txt')
+
+    # Parse the command-line arguments
+    return parser.parse_args()
+
+def main(args):
+    print(f"Parsing html {args.html_txt}")
+    soup = BeautifulSoup(open(args.html_txt,'r'),'html.parser')
+    containers = soup.find_all(*COMMENT_CONTAINER)
+    print(f"Found videos: {len(containers)}")
+    all_links = []
+    for container in containers:
+        links = container.find_all('a')
+        assert len(links)==1
+        all_links.append(links[0]['href'])
+        # for link in links:
+        #     assert
+        #     print(f"Url: {link['href']}")
+    with open(args.output_txt,'w') as f:
+        for link in all_links:
+            f.write(link)
+            f.write('\n')
+    print(f"Wrote to {args.output_txt}")
+if __name__=='__main__':
+    args = ap()
+    main(args)
